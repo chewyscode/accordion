8f3d17316339225a65bb1b0963958198735d0d4d
chu-young <ttheko101@gmail.com>
2021-03-13 17:20:01 +0200

    now complete

--- setup/app.js
+++ setup/app.js
@@ -1,10 +1,17 @@
  //traversing the dom
  const btns = document.querySelectorAll(".question-btn");
+ const questions = document.querySelectorAll(".question");
 
  btns.forEach(function (btn) {
    btn.addEventListener("click", function (e) {
      const question = e.currentTarget.parentElement.parentElement;
-
+  
      question.classList.toggle("show-text");
+
+     questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
    });
  });
